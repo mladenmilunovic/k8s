e977836b34b082822df237142e75fe9fdd1f5100
Mladen Milunovic <mladenmi@microsoft.com>
2020-10-19 20:47:03 +0200

    winsingle

---
+++ deploywindocker.tf
@@ -0,0 +1,151 @@
+# Create a resource group if it doesn’t exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "AzureWSSingleDockerHost"
+    location = "westeurope"
+
+    tags = {
+        environment = "AzureWS Demo"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "Sinergija19-vnet"
+    address_space       = ["10.0.0.0/24"]
+    location            = "westeurope"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "default"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.0.0/24"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "westeurope"
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "Server2016Docker-nsg"
+    location            = "westeurope"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    
+    security_rule {
+        name                       = "RDP"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3389"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+     
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "Server2016DockerNIC"
+    location                  = "westeurope"
+    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
+    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+
+    ip_configuration {
+        name                          = "Server2016DockerNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+    }
+    
+    byte_length = 8
+}
+
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                = "diag${random_id.randomId.hex}"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    location            = "westeurope"
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "2016Docker"
+    location              = "westeurope"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_D2S_v3"
+
+    delete_os_disk_on_termination = true
+    delete_data_disks_on_termination = true
+    
+    storage_os_disk {
+        name              = "Server2016DockerOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "MicrosoftWindowsServer"
+        offer     = "WindowsServer"
+        sku       = "2016-Datacenter-with-Containers"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "2016Docker"
+        admin_username = "mladen"
+        admin_password = "P@ssw0rd1234"
+    }
+
+    os_profile_windows_config {
+        enable_automatic_upgrades = false
+        provision_vm_agent = true
+    }
+    
+    boot_diagnostics {
+        enabled = "false"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+        }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
