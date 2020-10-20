c81522f8c0804ec5aceb11bbd0a791c0f5e1a8ef
Mladen Milunovic <mladenmi@microsoft.com>
2020-10-20 21:16:48 +0200

    port 80 and myip added

--- deploywindocker.tf
+++ deploywindocker.tf
@@ -66,7 +66,19 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "3389"
+        destination_port_range     = "178.148.188.246/32"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "Web"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
